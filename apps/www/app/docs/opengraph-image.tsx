cd6f71c1d5bf11b210191fedba4d6bebb80c4b41
Olivier Colonna <oliviercolonna@gmail.com>
2025-07-11 18:20:58 +0200

    fix: add opengraph-image to docs and root

---
+++ apps/www/app/docs/opengraph-image.tsx
@@ -0,0 +1,46 @@
+import { ImageResponse } from "next/og";
+import { readFile } from "node:fs/promises";
+import { join } from "node:path";
+
+export const alt = "About Acme";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = "image/png";
+
+export default async function Image() {
+  const interSemiBold = await readFile(
+    join(process.cwd(), "assets/Inter-SemiBold.ttf")
+  );
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 128,
+          background: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        usehooks.io documentation
+      </div>
+    ),
+    {
+      ...size,
+      fonts: [
+        {
+          name: "Inter",
+          data: interSemiBold,
+          style: "normal",
+          weight: 400,
+        },
+      ],
+    }
+  );
+}
