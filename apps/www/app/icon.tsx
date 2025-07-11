9e9ba15d2d33c011f958b79c3b6a27887498167e
Olivier Colonna <oliviercolonna@gmail.com>
2025-07-11 17:49:46 +0200

    fix: add icon

---
+++ apps/www/app/icon.tsx
@@ -0,0 +1,32 @@
+import { Anchor } from "lucide-react";
+import { ImageResponse } from "next/og";
+
+export const size = {
+  width: 32,
+  height: 32,
+};
+export const contentType = "image/png";
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 24,
+          background: "black",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          color: "white",
+        }}
+      >
+        <Anchor className="w-16 h-16" />
+      </div>
+    ),
+    {
+      ...size,
+    }
+  );
+}
